159e11821d2462eec8ded45720622d8abddb9d13
Divyanshu Singh <dsinecos@gmail.com>
2018-03-16 11:48:59 +0530

    Setup pagination using skip, sort and limit query modifiers

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -3,13 +3,17 @@ const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
 
-    let joe;
+    let joe, maria, alex, zach;
 
     beforeEach((done) => {
+        alex = new User({ name: 'Alex' });
         joe = new User({ name: 'Joe' });
-        joe
-            .save()
-            .then(() => done())
+        maria = new User({ name: 'Maria' });
+        zach = new User({ name: 'Zach' });
+
+        Promise
+            .all([alex.save(), joe.save(), maria.save(), zach.save()])
+            .then(() => done());
     })
 
     it("finds all users with the name of Joe", (done) => {
@@ -24,11 +28,26 @@ describe('Reading users out of the database', () => {
 
     it("Find a user with a particular ID", (done) => {
         User
-        .findOne({ _id: joe._id })
-        .then((user) => {
-            assert(user.name === 'Joe');
+            .findOne({ _id: joe._id })
+            .then((user) => {
+                assert(user.name === 'Joe');
+                done();
+            })
+    })
+
+    it("can skip and limit the result set", (done) => {
+        User
+        .find({})
+        .sort({ name: 1 })
+        .skip(1)
+        .limit(2)
+        .then((users) => {
+            assert(users[0].name === 'Joe');
+            assert(users[1].name === 'Maria');
+            assert(users.length === 2);
             done();
         })
+
     })
 
 })
